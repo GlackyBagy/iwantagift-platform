8b96a9e7e199acdaa4c33f2a6b40eec5a78113d8
GlackFag <99576022+GlackFag@users.noreply.github.com>
2026-01-29 16:15:51 +0300

    Init productprice and wishlist services

---
+++ productprice/src/test/java/online/iwantagift/api/productprice/ProductpriceApplicationTests.java
@@ -0,0 +1,13 @@
+package online.iwantagift.api.productprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductpriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
