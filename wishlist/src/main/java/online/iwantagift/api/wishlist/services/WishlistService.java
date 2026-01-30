d0567cdf5b7d2ca37b82320981d93ad2d2a8fe99
GlackFag <99576022+GlackFag@users.noreply.github.com>
2026-01-30 15:45:44 +0300

    refactor: replace save() with create; split findById() into 2 funcs: findById, findByIdOrThrow

--- wishlist/src/main/java/online/iwantagift/api/wishlist/services/WishlistService.java
+++ wishlist/src/main/java/online/iwantagift/api/wishlist/services/WishlistService.java
@@ -1,7 +1,11 @@
 package online.iwantagift.api.wishlist.services;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.PersistenceException;
 import lombok.RequiredArgsConstructor;
+import online.iwantagift.api.wishlist.exceptions.AlreadyExistsException;
 import online.iwantagift.api.wishlist.models.dto.abstracts.WishlistWriteDTO;
 import online.iwantagift.api.wishlist.models.dto.wl.WishlistPatchDTO;
 import online.iwantagift.api.wishlist.models.dto.wl.WishlistPutDTO;
@@ -11,15 +15,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class WishlistService {
+    @PersistenceContext
+    private EntityManager em;
     private final WishlistRepository listRepository;
 
-    public void save(Wishlist wishlist) {
-        listRepository.save(wishlist);
+    public void create(Wishlist wishlist) {
+        try {
+            em.persist(wishlist);
+            em.flush();
+        } catch (PersistenceException e) {
+            throw new AlreadyExistsException("Wishlist with id %s already exists"
+                    .formatted(wishlist.getId()), e);
+        }
     }
 
     @Transactional
@@ -36,22 +49,26 @@ public class WishlistService {
     }
 
     private void patch(WishlistPatchDTO dto) {
-        Wishlist wl = findById(dto.getId());
+        Wishlist wl = findByIdOrThrow(dto.getId());
 
         if (dto.getTitle() != null) wl.setTitle(dto.getTitle());
         if (dto.getDescription() != null) wl.setDescription(dto.getDescription());
     }
 
     private void put(WishlistPutDTO dto) {
-        Wishlist wl = findById(dto.getId());
+        Wishlist wl = findByIdOrThrow(dto.getId());
 
         wl.setTitle(dto.getTitle());
         wl.setDescription(dto.getDescription());
     }
 
-    public Wishlist findById(UUID id) {
+    public Wishlist findByIdOrThrow(UUID id) {
         return listRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Wishlist id %s not found".formatted(id)));
+                .orElseThrow(() -> new EntityNotFoundException("Wishlist with id %s not found".formatted(id)));
+    }
+
+    public Optional<Wishlist> findById (UUID id) {
+        return listRepository.findById(id);
     }
 
     public void deleteById(UUID id) {
