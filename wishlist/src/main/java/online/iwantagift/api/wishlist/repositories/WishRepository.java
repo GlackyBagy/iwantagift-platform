f87be226dc32cbd97e54a38582fa6f574c8966ca
GlackFag <99576022+GlackFag@users.noreply.github.com>
2026-02-05 07:37:16 +0300

    fix: use native sql query in findAllByOwnerId()

--- wishlist/src/main/java/online/iwantagift/api/wishlist/repositories/WishRepository.java
+++ wishlist/src/main/java/online/iwantagift/api/wishlist/repositories/WishRepository.java
@@ -1,8 +1,8 @@
 package online.iwantagift.api.wishlist.repositories;
 
 import online.iwantagift.api.wishlist.models.entities.Wish;
-import online.iwantagift.api.wishlist.models.entities.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +10,6 @@ import java.util.UUID;
 
 @Repository
 public interface WishRepository extends JpaRepository<Wish, UUID> {
+    @Query(value = "select * from wishes where owner_id = :ow", nativeQuery = true)
     List<Wish> findAllByOwnerId(UUID ow);
 }
