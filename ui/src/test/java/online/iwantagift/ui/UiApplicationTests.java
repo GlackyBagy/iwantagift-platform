677914dd36eb352b00607e816599ec1ee91726c4
GlackFag <99576022+GlackFag@users.noreply.github.com>
2026-04-02 14:39:21 +0300

    feat: add ui module

---
+++ ui/src/test/java/online/iwantagift/ui/UiApplicationTests.java
@@ -0,0 +1,13 @@
+package online.iwantagift.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
