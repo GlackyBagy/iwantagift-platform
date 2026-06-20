d9a82d83ecc6e4dedfa2abdefddd35ff6b338f3e
GlackFag <99576022+GlackFag@users.noreply.github.com>
2026-06-20 18:57:00 +0300

    feat(ui): add and modify tests

--- ui/src/main/java/online/iwantagift/ui/models/mappers/WishMapper.java
+++ ui/src/main/java/online/iwantagift/ui/models/mappers/WishMapper.java
@@ -8,8 +8,10 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface WishMapper {
     @Mapping(target = "wishlistId", source = "wishListId")
+    @Mapping(target = "defaultPrice", ignore = true) // todo pass to productPrice
     WishPayload toWishPayload(WishDTO dto);
 
     @Mapping(target = "wishListId", source = "wishlistId")
+    @Mapping(target = "createdAt", ignore = true)
     WishDTO toWishDTO(WishPayload payload);
 }
